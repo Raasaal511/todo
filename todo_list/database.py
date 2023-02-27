12234bebe328e68f893386909a86758a0ff619f2
Raasaal511 <raasaal511@gmail.com>
2023-02-27 10:27:01 +0300

    Connect to database

--- todo_list/database.py
+++ todo_list/database.py
@@ -3,10 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 CONNTECT_TO_DATABASE = str(dotenv_values('../.env')['CONNECT_TO_DATABASE'])
 
 engine = create_engine(CONNTECT_TO_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
